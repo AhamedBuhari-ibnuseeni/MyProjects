ae3d97f2da46389f9cd7ea0828aaad294c740253
AhamedBuhari-ibnuseeni <112266320+AhamedBuhari-ibnuseeni@users.noreply.github.com>
2022-09-04 12:40:14 +0530

    Add files via upload
    this is my first C program

---
+++ cprogram.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("Welcome to C programming");
+    int age=20;
+    char name[10]="Buhari";
+    printf("\nMy Name is :%s",name);
+    printf("\nMy Age is :%d",age);
+    char c='a';
+    char cc=100;
+    const double pi=3.14;
+    printf("\nMy PI value is :%lf",pi);
+    printf("\nThe size of pi value is :%d",sizeof(pi));
+}
+
